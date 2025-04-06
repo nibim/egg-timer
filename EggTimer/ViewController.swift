41a5a8aa3986807546fc3a6c887cf967aaaa74ce
nibim <nibm@kth.se>
2025-04-06 15:26:41 +0200

    Fixed: the "percentageProgress" is debugged, the problem was everytime the amount was printing was equal to zero because we did not cast secondsPassed & totalTime to float .

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -13,15 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLable: UILabel!
     
     @IBOutlet weak var cookedLevel: UIProgressView!
-    
     let eggTimes: [String: Int] = ["Soft": 3, "Medium": 420, "Hard": 720]
     var totalTime = 0
     var secondsPassed = 0
     var timer = Timer()
     @IBAction func hardnessSelected(_ sender: UIButton) {
-    
+        cookedLevel.progress = 0.0
+        secondsPassed = 0
         timer.invalidate()
         let hardness = sender.currentTitle!
+        titleLable.text = hardness
         totalTime = eggTimes[hardness]!
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
@@ -29,11 +30,9 @@ class ViewController: UIViewController {
     @objc func updateCounter() {
         //example functionality
         if secondsPassed < totalTime    {
-            
-            let percentageProgress = secondsPassed / totalTime
-            cookedLevel.progress = Float(percentageProgress)
-            
             secondsPassed += 1
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            cookedLevel.progress = percentageProgress
         }
         else {
             timer.invalidate()
