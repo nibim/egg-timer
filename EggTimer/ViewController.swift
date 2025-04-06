186225d0dd17c5453bf9818ea5d91a9ff8d29bdc
nibim <nibm@kth.se>
2025-04-06 15:08:55 +0200

    Feature: A ProgressView is set "cookedLevel" and it progress is tracked by using secondPassed and totalTime

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -12,20 +12,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var titleLable: UILabel!
     
+    @IBOutlet weak var cookedLevel: UIProgressView!
+    
     let eggTimes: [String: Int] = ["Soft": 3, "Medium": 420, "Hard": 720]
-    var secondsLeft = 60
+    var totalTime = 0
+    var secondsPassed = 0
     var timer = Timer()
     @IBAction func hardnessSelected(_ sender: UIButton) {
+    
         timer.invalidate()
         let hardness = sender.currentTitle!
-        secondsLeft = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
+    
     @objc func updateCounter() {
         //example functionality
-        if secondsLeft > 0 {
-            print("\(secondsLeft) seconds")
-            secondsLeft -= 1
+        if secondsPassed < totalTime    {
+            
+            let percentageProgress = secondsPassed / totalTime
+            cookedLevel.progress = Float(percentageProgress)
+            
+            secondsPassed += 1
         }
         else {
             timer.invalidate()
